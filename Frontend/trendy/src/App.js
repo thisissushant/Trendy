9b9626f1fce5c6e19a384c5ed0b2957bc3842459
Sushant Singh <sushantbibhu@gmail.com>
2024-01-08 01:05:22 +0530

    Added FontAwesome icons and integrated them into the navbar, add tailwind css for projrct style

--- Frontend/trendy/src/App.js
+++ Frontend/trendy/src/App.js
@@ -1,7 +1,8 @@
+import Navbar from "./Navbar";
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Navbar />
     </>
   );
 }
--- Frontend/trendy/tailwind.config.js
+++ Frontend/trendy/tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      Primary: ["Long Cang", "cursive"],
+      Secondary: ["Open Sans", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
